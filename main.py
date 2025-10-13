7cacb0c9daeb7d8ba6481fa08046a353c4569717
Tran Nguyen Gia Dat <tranlaluot.wkhl@gmail.com>
2025-10-13 12:54:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from telegram.ext import (
 )
 
 # === Import các chức năng còn tồn tại ===
-from features.autolike import autolike_command, list_autolike_command, cancel_autolike_command
+from features.auto_like_Ff import autolike_command, list_autolike_command, cancel_autolike_command
+
 from features.command_2fa import register_handlers as register_2fa_handlers
 # === Import các chức năng còn tồn tại ===
 from features.events_command import events_command
